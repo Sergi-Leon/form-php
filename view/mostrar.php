a11dc675a93350160f0b01758a03e54435be5ced
Sergio <100007099.joan23@fje.edu>
2022-02-02 11:34:10 +0100

    02/02/2022

---
+++ view/mostrar.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=, initial-scale=1.0">
+    <title>Recogida de datos</title>
+</head>
+<body>
+    <h1>Mostrar datos del formulario</h1>
+    <?php
+    echo '<p>'.$_REQUEST['nombre'].'</p>';
+    echo $_REQUEST['pwd'];
+    ?>
+</body>
+</html>
